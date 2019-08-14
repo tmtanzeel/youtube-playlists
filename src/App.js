ec902d867953e616668fc92f3f0d1ddf3a04c8ff
tmtanzeel <tmtanzeel9@gmail.com>
2019-08-14 23:50:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UCOYqNBjmWF8j-PzI7ZPjt_w&key=AIzaSyAMeZQW6sUQgLdDTnMVeokfbcFcT2A9SuA"
+    )
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json.items
+        });
+      });
+  }
+
+  render() {
+    var { isLoaded, items } = this.state;
+    var i=0;
+    if (!isLoaded) return <div>Loading...</div>;
+    return (
+      <div>
+        <ul>
+        {
+          items.map(item => (
+            <li key={item.id}>
+              Playlist: {items[i++].snippet.title}
+            </li>
+          ))
+        }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
